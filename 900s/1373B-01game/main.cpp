4a24fafdeda73bdef6518e854ba1c28119d115ea
Tejas Wilkhoo <30396273+tejaswilkhoo@users.noreply.github.com>
2020-12-19 21:24:47 -0500

    1373 in progress

--- 900s/1373B-01game/main.cpp
+++ 900s/1373B-01game/main.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
+
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int t;
+    int counter;
+
+    int zeros;
+    int ones;
+
+    string series;
+
+    cin>>series;
+
+
+    for(counter = 0; counter < series.length(); counter++)
+    {
+        if (series.at(counter) == "0")
+        {
+            cout<<something;
+        }
+    }
+
+
     return 0;
 }
