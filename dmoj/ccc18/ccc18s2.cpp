1b1b65f491df84432cadaadd053de0fce1be9935
Tejas Wilkhoo <30396273+tejaswilkhoo@users.noreply.github.com>
2021-02-05 19:21:51 -0500

    2018 s2

--- dmoj/ccc18/ccc18s2.cpp
+++ dmoj/ccc18/ccc18s2.cpp
@@ -1,1 +1,52 @@
+#include <bits/stdc++.h>
 
+using namespace std;  
+
+int main()
+{
+
+	int n;
+	cin>>n;
+	int arr[n][n];
+
+	for(int i=0; i<n; i++)
+	{
+		for(int j=0; j<n; j++)
+		{
+			cin>>arr[i][j];
+		}
+	}
+	
+	for(int a=0; a<n-1; a++)
+	{
+		if( (arr[0][a] > arr[0][a+1]) || (arr[a][0] > arr[a+1][0]) )
+		{
+			int temp[n][n];	
+			for(int i=0; i<n; i++)
+			{
+				for(int j=0; j<n; j++)
+				{
+					temp[i][j] = arr[i][j];
+				}
+			}
+			
+			for(int i=0; i<n; i++)
+			{
+				for(int j=0; j<n; j++)
+				{
+					arr[i][j] = temp[j][(n-i)-1];
+				}
+			}
+			a = -1;
+		}
+	}
+
+	for(int i=0; i<n; i++)
+	{
+		for(int j=0; j<n; j++)
+		{
+			cout<<arr[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+}
