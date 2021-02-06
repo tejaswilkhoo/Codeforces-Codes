be06b6871c71de76695e7771abaf656f3e433ac9
Tejas Wilkhoo <30396273+tejaswilkhoo@users.noreply.github.com>
2021-02-06 18:52:50 -0500

    17 s2

--- dmoj/ccc17/ccc17s2.cpp
+++ dmoj/ccc17/ccc17s2.cpp
@@ -15,25 +15,31 @@ int main()
 	
 	sort(arr, arr+n);
 	
-	cout<<endl;
-	for(int i=0; i<n; i++)
-	{
-		cout<<arr[i]<<" ";
-	}	
+	//cout<<endl;
+	//for(int i=0; i<n; i++)
+	//{
+		//cout<<arr[i]<<" ";
+	//}	
 	
 	int sortedarr[n/2][2];
 	
-	for(int i=0; i<(n/2); i++)
+	int g = (ceil(double(n)/2));
+	
+	for(int i=0; i<g; i++)
 	{
-		sortedarr[i][0] = arr[(n/2)-i-1];
-		sortedarr[i][1] = arr[(n/2)+i];
+		sortedarr[i][0] = arr[   g   -i-1];
+		sortedarr[i][1] = arr[   g   +i];
 	}
 	
 	
-	cout<<endl;
+//	cout<<endl;
 	for (int i=0; i<(n/2); i++)
 	{
 		cout<<sortedarr[i][0]<<" "<<sortedarr[i][1]<<" ";
+	}
+	if(n%2 == 1)
+	{
+		cout<<arr[0];
 	}
 	
 }
